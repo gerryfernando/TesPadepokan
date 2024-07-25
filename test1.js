bf365f68d8dab06d83b1c9fbc1ba90bc2e740b46
Gerry Fernando <firmanaldif3@gmail.com>
2024-07-25 09:15:32 +0700

    Solusi soal 1

--- test1.js
+++ test1.js
@@ -1,1 +1,24 @@
-const Tes = "HELLO";
+function checkCondition(var1, var2) {
+  return (
+    (var1 === "{" && var2 === "}") ||
+    (var1 === "[" && var2 === "]") ||
+    (var1 === "(" && var2 === ")")
+  );
+}
+
+function checkBalance(word) {
+  let check = true;
+  if (word.length % 2 === 1) {
+    check = false;
+  } else {
+    for (let index = 0; index < word.length / 2; index++) {
+      const j = word.length - 1 - index;
+      if (!checkCondition(word[index], word[j])) {
+        check = false;
+      }
+    }
+  }
+  return check;
+}
+
+console.log(checkBalance("({{}})"));
